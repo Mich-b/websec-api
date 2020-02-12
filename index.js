4d8c6353b1f12ff90bd10f1651132a6ed4ff5947
Mich-b <Mich-b@users.noreply.github.com>
2020-02-12 13:13:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 app.use(
     cors({
         credentials: true,
-        origin: 'https://www.michaelboeynaems.com'
+        origin: '*'
     })
 );
 app.options('*', cors());

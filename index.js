305eecc42bc0baacfc173f54c2472595bff7f367
Mich-b <Mich-b@users.noreply.github.com>
2022-02-16 16:53:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 app.use(
     cors({
         credentials: true,
-        origin: 'https://brave-boyd-6c3c10.netlify.app'
+        origin: 'https://cocky-snyder-5a02cf.netlify.app'
     })
 );
 app.options('*', cors());

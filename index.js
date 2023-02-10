49e496f2edab66b5452a91982cc52cab14a8d811
Michael Boeynaems <mboeynaems@outlook.com>
2023-02-10 17:44:29 +0100

    fix cors

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+var cors = require('cors');
 
+app.use(
+    cors({
+        credentials: true,
+        origin: 'http://127.0.0.1:5500',
+        methods: 'GET',
+        credentials: 'false',
+        })
+);
 
+app.options('*', cors());
 app.get('/', (req, res) => res.send('This api works.'));
-
 app.listen(process.env.PORT || 3000, function() {
     console.log('server running on port 3000', '');
 });

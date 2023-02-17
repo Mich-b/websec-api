f487cd1ac80a11864818c31a8961368fcedf6ed0
Michael Boeynaems <mboeynaems@outlook.com>
2023-02-17 14:42:01 +0100

    added post

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ app.use(
         credentials: true,
         origin: 'http://127.0.0.1:5500',
         methods: 'GET',
+        headers: 'none',
         credentials: 'false',
         })
 );
 
 app.options('*', cors());
 app.get('/', (req, res) => res.send('This api works.'));
+app.post('/',  (req, res) => res.send('This post works.'));
+
 app.listen(process.env.PORT || 3000, function() {
     console.log('server running on port 3000', '');
 });
